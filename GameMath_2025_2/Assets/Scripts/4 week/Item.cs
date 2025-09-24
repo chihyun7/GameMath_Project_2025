6562d4f6085c6334cc0435fbf6eac46fc1ffa60b
chihyun7 <clgus1603@gmail.com>
2025-09-24 15:38:16 +0900

    4강 탐색 알고리즘

---
+++ GameMath_2025_2/Assets/Scripts/4 week/Item.cs
@@ -0,0 +1,20 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+
+public class Item
+{
+    public string itemName;
+    public int quantity;
+
+    public Item(string name, int qty = 1)
+    {
+        itemName = name;
+        quantity = qty;
+    }   
+}
